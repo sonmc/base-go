471fde07f9208a19261a69a7fcee02843a80c61b
s-mai <sonmc@mor.com.vn>
2024-02-21 11:38:19 +0700

    use SIGTERM

--- application/router/mux_router.go
+++ application/router/mux_router.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -48,7 +49,6 @@ func (muxRouter *MuxRouter) SERVE(port string) {
 	go func() {
 		// Running the web server
 		err := s.ListenAndServe()
-
 		if err != nil {
 			muxRouter.logger.Println("Server shutting down on port ", port)
 			os.Exit(1)
@@ -57,19 +57,10 @@ func (muxRouter *MuxRouter) SERVE(port string) {
 
 	// Creating a channel
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	signal.Notify(c, os.Kill)
-
-	// Block until a signal is received.
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	sig := <-c
 	muxRouter.logger.Println("Receive signal:", sig)
-
-	// Gracefully shutdown / wait for client request to complete
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-
-	// Even though ctx will be expired, it is good practice to call its
-	// cancellation function in any case. Failure to do so may keep the
-	// context and its parent alive longer than necessary.
 	defer cancel()
 
 	// Shut down the server context
